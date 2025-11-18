a7d9c67ac034ec8562bfb0cb22efc24cf1bedf76
Emumba-Abdullah <abdullah.abrar@emumba.com>
2025-11-18 12:55:32 +0500

    chore: move icon to icons folder

--- src/components/CommunitySupportSection/index.tsx
+++ src/components/CommunitySupportSection/index.tsx
@@ -2,26 +2,9 @@ import React from "react";
 import * as LucideIcons from "lucide-react";
 import styles from "./CommunitySupportSection.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { TwitterXIcon } from "@site/src/theme/Icon/TwitterXIcon";
 
 // Custom Twitter X SVG Icon Component
-const TwitterXIcon: React.FC<{
-  size?: number;
-  className?: string;
-  style?: React.CSSProperties;
-}> = ({ size = 18, className = "", style = {} }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    fill="currentColor"
-    className={className}
-    style={{ ...style, minWidth: size }}
-    viewBox="0 0 16 16"
-  >
-    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-  </svg>
-);
-
 type CardAction = {
   label: string;
   href: string;
