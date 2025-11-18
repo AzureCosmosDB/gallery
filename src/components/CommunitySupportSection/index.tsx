913f9c19625da4ad0b5cc7c0b73184d4c8d8f1eb
Emumba-Abdullah <abdullah.abrar@emumba.com>
2025-11-18 12:30:03 +0500

    chore: fix twitters icon in the community support section

--- src/components/CommunitySupportSection/index.tsx
+++ src/components/CommunitySupportSection/index.tsx
@@ -3,6 +3,25 @@ import * as LucideIcons from "lucide-react";
 import styles from "./CommunitySupportSection.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
+// Custom Twitter X SVG Icon Component
+const TwitterXIcon: React.FC<{
+  size?: number;
+  className?: string;
+  style?: React.CSSProperties;
+}> = ({ size = 18, className = "", style = {} }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    fill="currentColor"
+    className={className}
+    style={{ ...style, minWidth: size }}
+    viewBox="0 0 16 16"
+  >
+    <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+  </svg>
+);
+
 type CardAction = {
   label: string;
   href: string;
@@ -124,9 +143,11 @@ const CommunitySupportSection = () => {
                   }
                 >
                   {card.actions.map((action, i) => {
-                    const ActionIcon = action.icon
-                      ? (LucideIcons as any)[action.icon]
-                      : null;
+                    const isTwitterX = action.icon === "X";
+                    const ActionIcon =
+                      action.icon && !isTwitterX
+                        ? (LucideIcons as any)[action.icon]
+                        : null;
                     const isOutlined = action.variant === "outlined";
                     const fullWidth =
                       action.fullWidth || card.title === "Contact Us";
@@ -159,9 +180,11 @@ const CommunitySupportSection = () => {
                             : undefined
                         }
                       >
-                        {ActionIcon && (
+                        {isTwitterX ? (
+                          <TwitterXIcon size={18} />
+                        ) : ActionIcon ? (
                           <ActionIcon size={18} style={{ minWidth: 18 }} />
-                        )}
+                        ) : null}
                         {action.label}
                       </a>
                     );
