645b73c7ab5147f4ea5d605d75d152efd40020a9
Emumba-Abdullah <abdullah.abrar@emumba.com>
2026-03-26 15:36:50 +0500

    fix: correct color property syntax for Video button style

--- src/utils/buttonStyleUtils.ts
+++ src/utils/buttonStyleUtils.ts
@@ -26,7 +26,7 @@ const BUTTON_STYLE_MAP: Record<string, ButtonStyles> = {
   Video: {
     border: "1px solid #A9B3D8",
     backgroundColor: "#EDF1FF",
-    color: "#3357E4;",
+    color: "#3357E4",
     borderRadius: "3px 3px 14px 14px",
   },
   Training: {
